1da91975be17a4618e2f0be17972d496818e7d81
mavazal <mvazquezalonso@gmail.com>
2017-10-15 22:31:02 +0200

    First Commit. Template Master Detail Portable. Add Gorilla player (FrontEnd) and Prism.

---
+++ HelloXamarin/HelloXamarin.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloXamarin.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new HelloXamarin.App());
+        }
+    }
+}
